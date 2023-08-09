de933c44f2a484d2fdb389ce0f31d224dc1054a5
vimal199 <vimalbhardwaj8@gmail.com>
2023-08-09 19:39:16 +0530

    Adding react components of phonebook

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 /*const requestLogger = (request, response, next) => {
     console.log('Method: ', request.method);
     console.log('Path: ', request.path);
